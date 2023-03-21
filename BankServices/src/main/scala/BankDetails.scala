5ef0fe254c6d0641e80ad65f5a27414da6424559
Akhil1857 <akhil.trivedi@knoldus.com>
2023-03-21 23:40:37 +0530

    Final

---
+++ BankServices/src/main/scala/BankDetails.scala
@@ -0,0 +1,52 @@
+import scala.collection.mutable
+import scala.util.Random
+
+case class Transactions(transactionId: Long, accountNumber: Long, transactionType: String, amount: Double)
+
+class BankDetails {
+  private val accounts: mutable.Map[Long, Double] = mutable.Map()
+
+  //creates the account of the new user.
+  def createAccount(openingBalance: Double): mutable.Map[Long, Double] = {
+    val accountNumber = Random.nextLong().abs
+    accounts += (accountNumber -> openingBalance)
+    accounts
+  }
+
+  def listAccounts(): mutable.Map[Long, Double] = {
+    accounts
+  }
+
+  //fetchAccountBalance() will return the account balance.
+  def fetchAccountBalance(accountNumber: Long): Double = {
+    accounts.getOrElse(accountNumber, 0.0)
+  }
+
+  //updateBalance() is used to perform the Debit/Credit
+  def updateBalance(transactions: List[Transactions]): mutable.Map[Long, Double] = {
+    transactions.foreach(transaction => {
+      val accountNumber = transaction.accountNumber
+      val transactionAmount = transaction.amount
+      transaction.transactionType match {
+        case "credit" => val newBalance = fetchAccountBalance(accountNumber) + transactionAmount
+          accounts += (accountNumber -> newBalance)
+
+        case "debit" => val currentBalance = fetchAccountBalance(accountNumber)
+          if (currentBalance >= transactionAmount) {
+            val newBalance = currentBalance - transactionAmount
+            accounts += (accountNumber -> newBalance)
+          }
+      }
+    })
+    accounts
+  }
+
+  // deleteAccount deletes the account of the user.
+  def deleteAccount(accountNumber: Long): Boolean = {
+    if (accounts.contains(accountNumber)) {
+      accounts -= accountNumber
+      true
+    }
+    else false
+  }
+}
---
+++ BankServices/src/test/scala/BankDetailsTestcase.scala
@@ -0,0 +1,37 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.collection.mutable
+import scala.util.Random
+
+class BankDetailsTestcase extends AnyFunSuite {
+  val BankService = new BankDetails
+  //Some stored Account in the Bank
+  BankService.createAccount(10000)
+  BankService.createAccount(20000)
+  BankService.createAccount(25000)
+  BankService.createAccount(20000)
+
+
+  val accountsBeforeOperations: mutable.Map[Long, Double] = BankService.listAccounts()
+  val accountNumberBeforeOperations: List[Long] = accountsBeforeOperations.keySet.toList
+
+  test("fetchAccountBalance method should return the balance in the account") {
+    val currentBalance: Double = BankService.fetchAccountBalance(accountNumberBeforeOperations.head)
+    assert(BankService.fetchAccountBalance(accountNumberBeforeOperations.head) == currentBalance)
+  }
+  test("Credit:-update balance should perform operation based on the list of the transactions") {
+    val transaction: List[Transactions] = List(Transactions(Random.nextLong().abs, accountNumberBeforeOperations(1), "credit", 1000))
+    val beforeCredit: Double = BankService.fetchAccountBalance(accountNumberBeforeOperations(1))
+    val afterCredit: mutable.Map[Long, Double] = BankService.updateBalance(transaction)
+    assert(afterCredit(accountNumberBeforeOperations(1)) > beforeCredit)
+  }
+  test("Debit:-update balance should perform operation based on the list of the transactions") {
+    val transaction: List[Transactions] = List(Transactions(Random.nextLong().abs, accountNumberBeforeOperations(2), "debit", 1000))
+    val beforeDebit: Double = BankService.fetchAccountBalance(accountNumberBeforeOperations(2))
+    val afterDebit: mutable.Map[Long, Double] = BankService.updateBalance(transaction)
+    assert(afterDebit(accountNumberBeforeOperations(2)) < beforeDebit)
+  }
+  test("deleteAccount should safely delete the account") {
+    assert(BankService.deleteAccount(accountNumberBeforeOperations(1)) == true)
+  }
+}
